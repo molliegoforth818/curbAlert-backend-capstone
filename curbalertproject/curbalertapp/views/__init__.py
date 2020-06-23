985ccad2f327c4a12f3e4363b417f65a49c31662
Mollie Goforth <molliegoforth818@gmail.com>
2020-06-23 18:31:57 -0500

    installed safedelete

--- curbalertproject/curbalertapp/views/__init__.py
+++ curbalertproject/curbalertapp/views/__init__.py
@@ -5,4 +5,5 @@ from .auth.register import Alerter
 from .donations.donation_form import donation_form
 from .donations.donation_list import donation_list
 from .donations.donation_form import donation_edit_form
+from .donations.donation_detail import donation_details
 
